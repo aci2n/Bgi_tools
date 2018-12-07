a0404b9854f17ab72ed2ee3cbce4b8f511bf3455
alvarocalace@hotmail.com <alvarocalace@hotmail.com>
2018-12-07 15:36:37 -0300

    make poreader prefixable

--- poreader.py
+++ poreader.py
@@ -5,7 +5,7 @@ import os.path
 import json
 
 class Reader():
-    def read(self, directory):
+    def read(self, directory, prefix):
         voiceDict = {}
         exp = directory + '/**/*.pot'
 
@@ -18,16 +18,16 @@ class Reader():
                 voiceEntries = filter(lambda entry: entry.comment == 'VOICE', entries)
                 
                 for voiceEntry in voiceEntries:
-                    voiceDict[voiceEntry.msgctxt + '.ogg'] = voiceEntry.msgstr
+                    voiceDict[prefix + voiceEntry.msgctxt + '.ogg'] = voiceEntry.msgstr
 
         return voiceDict
 
-    def main(self, directory):
-        entries = self.read(directory)
+    def main(self, directory, prefix):
+        entries = self.read(directory, prefix)
         file = os.path.join(directory, 'entries.json')
 
         with open(file, 'w') as stream:
             json.dump(entries, stream)
 
-if __name__ == '__main__' and len(sys.argv) == 2:
-    Reader().main(sys.argv[1])
+if __name__ == '__main__' and len(sys.argv) == 3:
+    Reader().main(sys.argv[1], sys.argv[2])
